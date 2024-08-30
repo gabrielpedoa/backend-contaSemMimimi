3af97c5879fc04ac70f1dba402cd5411670ef93d
GABRIEL ANTUNES <gabrielpedoa@gmail.com>
2024-08-30 11:07:02 -0300

    income category repository

---
+++ src/@types/income-category-type.ts
@@ -0,0 +1,8 @@
+type IIncomeCategory = {
+  id_category?: number;
+  name: string;
+  description: string;
+  created_at: Date;
+  updated_at: Date;
+  active: boolean;
+};
--- src/infra/repositories/income-category.ts
+++ src/infra/repositories/income-category.ts
@@ -1,7 +1,37 @@
+import prisma from "../prisma";
+
 export class IncomeCategoryRepository {
-  public async create() {}
-  public async loadAll() {}
-  public async loadById() {}
-  public async update() {}
-  public async delete() {}
+  public async create(
+    data: Omit<IIncomeCategory, "id_category">
+  ): Promise<IIncomeCategory> {
+    return await prisma.income_category.create({
+      data: { ...data },
+    });
+  }
+
+  public async loadAll() {
+    return await prisma.income_category.findMany({
+      where: { active: true },
+    });
+  }
+
+  public async loadById(id: number) {
+    return await prisma.income_category.findFirst({
+      where: { id_category: id },
+    });
+  }
+
+  public async update(data: IIncomeCategory) {
+    return await prisma.income_category.update({
+      data: { ...data },
+      where: { id_category: data.id_category },
+    });
+  }
+
+  public async delete(id: number) {
+    return await prisma.income_category.update({
+      data: { active: false },
+      where: { id_category: id },
+    });
+  }
 }
